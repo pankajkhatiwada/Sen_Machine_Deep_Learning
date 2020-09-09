79735ba59f8a3406afb7219639c885fcaaf184d7
Pankaj <pankajkhatiwada@gmail.com>
2020-09-09 16:37:00 +0200

    DF Ready

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-ptint("pankaj")
+import pandas as pd
+import quandl
+
+df = quandl.get("WIKI/GOOGL")
+
+df = df[["Adj. Open", "Adj. High", "Adj. Low", "Adj. Close", "Adj. Volume"]]
+
+df["HCL_PCT"] = (df["Adj. High"] - df["Adj. Close"]) / df["Adj. Close"] * 100
+
+df["PCT_change"] = (df["Adj. Close"] - df["Adj. Open"]) / df["Adj. Open"] * 100
+
+df = df[["Adj. Close", "HCL_PCT", "PCT_change", "Adj. Volume"]]
+
+print(df.head())
